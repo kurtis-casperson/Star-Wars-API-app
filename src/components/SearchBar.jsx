74f803e875b86d4f0fe8d29a5028a36a6660353c
Kurtis <kcasperson7@gmail.com>
2023-05-21 20:04:47 -0700

    Add clear search button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,22 @@ import { Button } from 'react-bootstrap'
 const SearchBar = (props) => {
   const { setRequestUrl, searchInput, setSearchInput } = props
   const handleInputSearch = (e) => {
+    e.preventDefault()
     setSearchInput(e.target.value)
   }
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    if (searchInput === '' || null || undefined)
+      return setRequestUrl('https://swapi.py4e.com/api/people/')
+    setRequestUrl(`https://swapi.py4e.com/api/people/?search=${searchInput}`)
+  }
+
+  const handleClearSearch = (e) => {
+    e.preventDefault()
+    setRequestUrl('https://swapi.py4e.com/api/people/')
+    setSearchInput('')
+  }
   return (
     <>
       <div className="search-bar-container">
@@ -23,15 +36,19 @@ const SearchBar = (props) => {
           type="button"
           variant="info"
           size="sm"
-          onClick={(e) => {
-            if (searchInput === '' || null || undefined)
-              return setRequestUrl('https://swapi.dev/api/people')
-            handleInputSearch(e)
-            setSearchInput('')
-            setRequestUrl(`https://swapi.dev/api/people/?search=${searchInput}`)
-          }}
+          onClick={handleSearchSubmit}
         >
           Search
+        </Button>
+
+        <Button
+          id="clearButton"
+          type="button"
+          variant="info"
+          size="sm"
+          onClick={handleClearSearch}
+        >
+          Clear
         </Button>
       </div>
     </>
